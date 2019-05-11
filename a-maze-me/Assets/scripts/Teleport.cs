be879cdf5ad8129962f4d6729ebedc8f8cb9fe6f
Kaith Menken <kaith-uwe.menken@uni-ulm.de>
2019-05-12 00:25:31 +0200

    Only Tom Cruise knows why.

---
+++ a-maze-me/Assets/scripts/Teleport.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Teleport : MonoBehaviour
+{
+public GameObject currentMaze;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        currentMaze.transform.Rotate(0,-90,0, Space.World);
+    }
+}
