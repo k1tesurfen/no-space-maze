c70f116745566eb7a8fcaedd4e6be8e5b91a7ae9
Kaith Menken <kaith-uwe.menken@uni-ulm.de>
2019-05-15 09:45:02 +0200

    first test maze is finished.

---
+++ a-maze-me/Assets/scripts/Change.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change : MonoBehaviour
+{
+    public GameObject next;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        Debug.Log("Triggered by: " + other.gameObject.name);
+        transform.parent.gameObject.SetActive(false);
+        next.SetActive(true);
+    }
+}
