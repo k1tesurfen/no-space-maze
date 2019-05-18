8f1335b3ae9eeced760b81e35ff8af3ef3b2aaa8
Kaith Menken <kaith-uwe.menken@uni-ulm.de>
2019-05-18 08:11:35 +0200

    created new scene for laser demonstation

---
+++ a-maze-me/Assets/scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject laser;
+    public float timer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        (Instantiate(laser) as GameObject).GetComponent<Laser>().StartLaser(new Vector3(-10, Random.Range(1f, 2f), 0), 20f, new Vector3(1, 0, 0), 0.4f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 5f)
+        {
+            //this line will instanciate the prefab and then 
+            //call the StartLaser-Method from the script that is attatched to the prefab
+            (Instantiate(laser) as GameObject).GetComponent<Laser>().StartLaser(new Vector3(-10, Random.Range(1f, 2f), 0), 20f, new Vector3(1, 0, 0), 0.4f);
+            timer = 0f;
+        }
+    }
+}
---
+++ a-maze-me/Assets/scripts/Laser.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    //position where the laser will start.
+    public Vector3 startPosition;
+
+    //Distance, the laser will travel before destruction
+    public float travelDistance;
+
+    //Direction in which the laser should move ( (1,0,0) or (0,0,1) is recommended)
+    public Vector3 movingDirection;
+
+    //The speed in which the laser will move
+    public float movingSpeed;
+
+    //Startpoint of the laserbeam
+    private Vector3 laserStart;
+
+    //Endpoint of the laserbeam
+    private Vector3 laserEnd;
+    LineRenderer lr;
+    bool laserCreated = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateLaser();
+    }
+
+    public void UpdateLaser()
+    {
+        if (laserCreated)
+        {
+            //we decrease the traveldistance every frame and destroy the laser if it is below 0
+            if (travelDistance < 0)
+            {
+                DestroyLaser();
+            }
+
+            //set new position of the gameobject which holds the laser
+            gameObject.transform.position = transform.position + movingSpeed * movingDirection;
+
+            //set new start and endpositions for the laserbeam
+            laserStart = laserStart + movingSpeed * movingDirection;
+            laserEnd = laserEnd + movingSpeed * movingDirection;
+            lr.SetPosition(0, laserStart);
+            lr.SetPosition(1, laserEnd);
+
+            //decrease distance, which the laser is allowed to travel
+            travelDistance -= movingSpeed;
+        }
+    }
+    public void StartLaser(Vector3 start, float distance, Vector3 direction, float speed)
+    {
+        //set the global variables of this gameobject
+        this.startPosition = start;
+        this.movingDirection = direction;
+        this.movingSpeed = speed;
+        this.travelDistance = distance;
+
+        //get the line renderer component of this gameobject
+        lr = gameObject.GetComponent<LineRenderer>();
+
+        //set the startposition and the laser beam start and endpoints
+        gameObject.transform.position = startPosition;
+        laserStart = start + (500 * (Quaternion.AngleAxis(90, Vector3.up) * direction));
+        laserEnd = start + (-500 * (Quaternion.AngleAxis(90, Vector3.up) * direction));
+
+        //enable the linerenderer to make it visible
+        lr.enabled = true;
+        laserCreated = true;
+    }
+
+    public void DestroyLaser()
+    {
+        Destroy(gameObject);
+    }
+}
